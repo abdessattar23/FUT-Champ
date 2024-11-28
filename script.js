1c38c799f1f67235f9defdb6e53699d7b20f8f57
abdessattar23 <elyagoubiabdessattar@gmail.com>
2024-11-28 17:13:24 +0100

    fixing code

--- script.js
+++ script.js
@@ -102,8 +102,9 @@ playersdata.onreadystatechange = function () {
 
     div.addEventListener('click', function () {
         let positionElement = document.querySelector(`[position="${player.position}"]`);
-        positionElement.classList.remove("blackcard")
-        positionElement.classList.add("goldcard")
+        positionElement.classList.remove("blackcard");
+        positionElement.classList.add("goldcard");
+        if(player.position !== "GK"){
           positionElement.innerHTML = `
             <div class="flex">
               <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
@@ -143,18 +144,248 @@ playersdata.onreadystatechange = function () {
               <img src="${player.flag}" alt="${player.name}">
               <img src="${player.logo}" alt="${player.club}">
             </div>
-          `;
+          `
+      }else{
+          positionElement.innerHTML = `
+            <div class="flex">
+              <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
+                <span class="mb-[-5px] font-bold">${player.rating}</span>
+                <span class="text-[10px] font-medium">${player.position}</span>
+              </div>
+              <img class="w-20" src="${player.photo}" alt="${player.name}">
+            </div>
+            <p class="font-Raleway text-[11px] font-bold text-[#362f16] mb-[-4px]">${player.name}</p>
+            <div class="text-[#362f16] gap-1 flex">
+              <div class="flex flex-col gap-0 justify-center items-center">
+                <span class=" text-[7px] font-medium mb-[-4px]">DIV</span>
+                <span class="font-bold text-[10px]">${player.diving}</span>
+              </div>
+              <div class="flex flex-col gap-0 justify-center items-center">
+                <span class=" text-[7px] font-medium mb-[-4px]">HAN</span>
+                <span class="font-bold text-[10px]">${player.handling}</span>
+              </div>
+              <div class="flex flex-col gap-0 justify-center items-center">
+                <span class=" text-[7px] font-medium mb-[-4px]">KIC</span>
+                <span class="font-bold text-[10px]">${player.kicking}</span>
+              </div>
+              <div class="flex flex-col gap-0 justify-center items-center">
+                <span class=" text-[7px] font-medium mb-[-4px]">REF</span>
+                <span class="font-bold text-[10px]">${player.reflexes}</span>
+              </div>
+              <div class="flex flex-col gap-0 justify-center items-center">
+                <span class=" text-[7px] font-medium mb-[-4px]">SPE</span>
+                <span class="font-bold text-[10px]">${player.speed}</span>
+              </div>
+              <div class="flex flex-col gap-0 justify-center items-center">
+                <span class=" text-[7px] font-medium mb-[-4px]">POS</span>
+                <span class="font-bold text-[10px]">${player.positioning}</span>
+              </div>
+            </div>
+            <div class="flex justify-center items-center w-3 gap-2">
+              <img src="${player.flag}" alt="${player.name}">
+              <img src="${player.logo}" alt="${player.club}">
+            </div>
+          `
+      }
         
         
         document.getElementById("modalfilter").classList.add("hidden");
         document.getElementById("modalfilter").classList.remove("flex");
+        data = data.filter(playerss => {
+                return playerss.name !== player.name;
+        })
       });
   
      
       filtredPlayersCase.appendChild(div);
     });
   }
+  data.forEach(player => {
+    let div = document.createElement("div")
+    div.setAttribute("class", "cursor-pointer goldcard bg-no-repeat bg-center bg-cover w-32 h-44 flex flex-col pt-8 items-center")
+    if(player.position !== "GK"){
+    div.innerHTML = `
+      <div class="flex">
+        <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
+          <span class="mb-[-5px] font-bold">${player.rating}</span>
+          <span class="text-[10px] font-medium">${player.position}</span>
+        </div>
+        <img class="w-20" src="${player.photo}" alt="${player.name}">
+      </div>
+      <p class="font-Raleway text-[11px] font-bold text-[#362f16] mb-[-4px]">${player.name}</p>
+      <div class="text-[#362f16] gap-1 flex">
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">PAC</span>
+          <span class="font-bold text-[10px]">${player.pace}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">SHO</span>
+          <span class="font-bold text-[10px]">${player.shooting}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">PAS</span>
+          <span class="font-bold text-[10px]">${player.passing}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">DRI</span>
+          <span class="font-bold text-[10px]">${player.dribbling}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">DEF</span>
+          <span class="font-bold text-[10px]">${player.defending}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">PHY</span>
+          <span class="font-bold text-[10px]">${player.physical}</span>
+        </div>
+      </div>
+      <div class="flex justify-center items-center w-3 gap-2">
+        <img src="${player.flag}" alt="${player.name}">
+        <img src="${player.logo}" alt="${player.club}">
+      </div>
+    `
+}else{
+    div.innerHTML = `
+      <div class="flex">
+        <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
+          <span class="mb-[-5px] font-bold">${player.rating}</span>
+          <span class="text-[10px] font-medium">${player.position}</span>
+        </div>
+        <img class="w-20" src="${player.photo}" alt="${player.name}">
+      </div>
+      <p class="font-Raleway text-[11px] font-bold text-[#362f16] mb-[-4px]">${player.name}</p>
+      <div class="text-[#362f16] gap-1 flex">
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">DIV</span>
+          <span class="font-bold text-[10px]">${player.diving}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">HAN</span>
+          <span class="font-bold text-[10px]">${player.handling}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">KIC</span>
+          <span class="font-bold text-[10px]">${player.kicking}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">REF</span>
+          <span class="font-bold text-[10px]">${player.reflexes}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">SPE</span>
+          <span class="font-bold text-[10px]">${player.speed}</span>
+        </div>
+        <div class="flex flex-col gap-0 justify-center items-center">
+          <span class=" text-[7px] font-medium mb-[-4px]">POS</span>
+          <span class="font-bold text-[10px]">${player.positioning}</span>
+        </div>
+      </div>
+      <div class="flex justify-center items-center w-3 gap-2">
+        <img src="${player.flag}" alt="${player.name}">
+        <img src="${player.logo}" alt="${player.club}">
+      </div>
+    `
+}
 
+div.addEventListener('click', function () {
+    let positionElement = document.querySelector(`[position="${player.position}"]`);
+    positionElement.classList.remove("blackcard");
+    positionElement.classList.add("goldcard");
+    if(player.position !== "GK"){
+      positionElement.innerHTML = `
+        <div class="flex">
+          <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
+            <span class="mb-[-5px] font-bold">${player.rating}</span>
+            <span class="text-[10px] font-medium">${player.position}</span>
+          </div>
+          <img class="w-20" src="${player.photo}" alt="${player.name}">
+        </div>
+        <p class="font-Raleway text-[11px] font-bold text-[#362f16] mb-[-4px]">${player.name}</p>
+        <div class="text-[#362f16] gap-1 flex">
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">PAC</span>
+            <span class="font-bold text-[10px]">${player.pace}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">SHO</span>
+            <span class="font-bold text-[10px]">${player.shooting}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">PAS</span>
+            <span class="font-bold text-[10px]">${player.passing}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">DRI</span>
+            <span class="font-bold text-[10px]">${player.dribbling}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">DEF</span>
+            <span class="font-bold text-[10px]">${player.defending}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">PHY</span>
+            <span class="font-bold text-[10px]">${player.physical}</span>
+          </div>
+        </div>
+        <div class="flex justify-center items-center w-3 gap-2">
+          <img src="${player.flag}" alt="${player.name}">
+          <img src="${player.logo}" alt="${player.club}">
+        </div>
+      `
+  }else{
+      positionElement.innerHTML = `
+        <div class="flex">
+          <div class="flex flex-col mr-[-8px] text-[#362f16] items-center">
+            <span class="mb-[-5px] font-bold">${player.rating}</span>
+            <span class="text-[10px] font-medium">${player.position}</span>
+          </div>
+          <img class="w-20" src="${player.photo}" alt="${player.name}">
+        </div>
+        <p class="font-Raleway text-[11px] font-bold text-[#362f16] mb-[-4px]">${player.name}</p>
+        <div class="text-[#362f16] gap-1 flex">
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">DIV</span>
+            <span class="font-bold text-[10px]">${player.diving}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">HAN</span>
+            <span class="font-bold text-[10px]">${player.handling}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">KIC</span>
+            <span class="font-bold text-[10px]">${player.kicking}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">REF</span>
+            <span class="font-bold text-[10px]">${player.reflexes}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">SPE</span>
+            <span class="font-bold text-[10px]">${player.speed}</span>
+          </div>
+          <div class="flex flex-col gap-0 justify-center items-center">
+            <span class=" text-[7px] font-medium mb-[-4px]">POS</span>
+            <span class="font-bold text-[10px]">${player.positioning}</span>
+          </div>
+        </div>
+        <div class="flex justify-center items-center w-3 gap-2">
+          <img src="${player.flag}" alt="${player.name}">
+          <img src="${player.logo}" alt="${player.club}">
+        </div>
+      `
+  }
+    
+    
+    document.getElementById("modalfilter").classList.add("hidden");
+    document.getElementById("modalfilter").classList.remove("flex");
+    data = data.filter(playerss => {
+            return playerss.name !== player.name;
+    })
+  });
+
+ 
+  document.getElementById("all").appendChild(div);
+});
     
     document.querySelectorAll('.position-button').forEach(button => {
       button.addEventListener('click', function () {
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js",
+    "./main.css",
+    // Add any other paths to your HTML or JS files here
+  ],
+  theme: {
+    extend: {
+  },
+  plugins: [],
+}
+}
